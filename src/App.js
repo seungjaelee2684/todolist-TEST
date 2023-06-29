dd6b74a47b1d962429ee42c8e37931d75d322d8d
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-06-29 10:40:15 +0900

    todolist TEST

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState([{
+    id: 1,
+    text: "react를 배워봅시다."
+  }])
+  
+  const [text, setText] = useState('');
+
+  const iuputContent = (event) => {
+    setText(event.target.value);
+  }
+
+  const clickBtn = () => {
+    const newTodo = {
+      id: todo.length + 1,
+      text
+    };
+
+    setTodo([...todo, newTodo]);
+    setText('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className='App-inputBox'>
+        <input
+          className='App-input'
+          value={text}
+          onChange={iuputContent}
         >
-          Learn React
-        </a>
+        </input>
+        <button
+          className='App-btn' 
+          onClick={clickBtn}>
+          추가하기
+        </button>
+      </div>
+      <header className="App-header">
+        My Todo List
       </header>
+      <div
+        key={todo.id}
+      >
+        {todo.map((item) => {
+          return (
+            <div className='App-text'>
+              {item.text}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
